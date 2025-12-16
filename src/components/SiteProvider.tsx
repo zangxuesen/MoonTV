6888d14b72adb84dac6d2c227b6bb6d8fd93f9d1
zangxuesen <52036102+zangxuesen@users.noreply.github.com>
2025-12-16 15:26:14 +0800

    Change default site name and announcement message

--- src/components/SiteProvider.tsx
+++ src/components/SiteProvider.tsx
@@ -4,9 +4,9 @@ import { createContext, ReactNode, useContext } from 'react';
 
 const SiteContext = createContext<{ siteName: string; announcement?: string }>({
   // 默认值
-  siteName: 'MoonTV',
+  siteName: 'baimeiTV',
   announcement:
-    '本网站仅提供影视信息搜索服务，所有内容均来自第三方网站。本站不存储任何视频资源，不对任何内容的准确性、合法性、完整性负责。',
+    '欢迎使用，请勿对本网站进行任何形式的传播和分享。',
 });
 
 export const useSite = () => useContext(SiteContext);
